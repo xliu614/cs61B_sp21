ed0fda5d35fd49bdf1482edc9eb86b6baedea3ff
tao2tao <roiwang@gmail.com>
2024-09-22 11:49:12 -0700

    Added print function, where x is an integer.

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -2,6 +2,11 @@ public class HelloWorld
 {
     // OK jdsajdasjk\\\
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int x = 10;
+        System.out.println("The value of x is " + x);
+
 	}
+
 } 
