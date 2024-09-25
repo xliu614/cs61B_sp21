20fab8309d71290117a115b3a61a70c762719ef5
tao2tao <roiwang@gmail.com>
2024-09-24 20:48:36 -0700

    Added print function, where x is an integer.

--- HW0/ArrayExercise.java
+++ HW0/ArrayExercise.java
@@ -1,3 +1,17 @@
 // create a ArrayExcercise class
 // create an int array which has 10 ints
 // display all the ints at odd positions in the array
+public class ArrayExercise {
+    public static void calc(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] test_Array = {32,2,3,4,5,6,7,8,49,10,3,4,5,6,7,8,9};
+        calc(test_Array);
+    }
+}
