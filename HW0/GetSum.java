9ebac46c47d2517f98f4ea9cb8d53e0ed26644a0
tao2tao <roiwang@gmail.com>
2024-10-22 20:49:00 -0700

    Added two functions to GetSum.java

--- HW0/GetSum.java
+++ HW0/GetSum.java
@@ -1,2 +1,19 @@
 // For an int array, get the sum of all the elements
+
 // and get the sum of all the elements except the first and the last element
+public class GetSum {
+
+    public static int getArraySum (int[] arr) {
+        int reval = 0;
+        for (int i = 0; i < arr.length; i++){
+            reval = reval + arr[i];
+        }
+        return reval;
+
+    }
+    public static void main(String[] args) {
+        int[] test_Array = {1,2,3,4,5};
+        int result = getArraySum(test_Array);
+        System.out.println(result);
+    }
+}
